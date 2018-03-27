a86c9c2e11e67773647fcc439d7e0ea4a6badee1
Samuel Breese <samuel@chame.co>
2018-03-27 14:00:25 -0400

    Add procfile

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,11 @@ module Main where
 
 import Good.Prelude
 
+import System.Environment (getEnv)
+
 import Good.Utilities.Web
 import Good.API.SIS
 
 main :: IO ()
-main = serving 3000 sis
+main = do (Just port) <- readMay <$> getEnv "PORT"
+          serving port sis
