77a8f631aacb03806b3e338a4f227331913c34e9
Samuel Breese <samuel@chame.co>
2018-07-10 17:34:48 -0400

    Add possessions to character generator

--- src/Good/Services/TL7.hs
+++ src/Good/Services/TL7.hs
@@ -32,14 +32,23 @@ character = do
   appearance <- rollAppearance
   trait <- rollTrait
   personality <- rollPersonality
+  posessions <- roll (Sides 6) >>= items
   pure $ mconcat
     [ H.div ! A.class_ "outline-2" $ H.ul $ mconcat
       [ H.li $ mconcat [H.strong "Background: ", H.toHtml background]
       , H.li $ mconcat [H.strong "Appearance: ", H.toHtml appearance]
       , H.li $ mconcat [H.strong "Trait: ", H.toHtml trait]
       , H.li $ mconcat [H.strong "Personality: ", H.toHtml personality]
+      , H.li $ mconcat [H.strong "Posessions: ", H.ul $ mconcat posessions]
       ]
     ]
+
+items :: MonadIO m => Integer -> m [H.Html]
+items n | n <= 0 = pure []
+        | otherwise = (:) <$> item <*> items (pred n)
+
+item :: MonadIO m => m H.Html
+item = H.li . H.toHtml <$> rollItem
 
 data Die where
   Sides :: Integer -> Die
